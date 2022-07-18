762db85935b8263f233f46211600210a0f878832
Roman-Ojha <razzroman98@gmail.com>
2022-07-18 22:47:15 +0545

    fix: using poetry for package manager

--- Tut_02_MongoDB_Integration.py
+++ Tut_02_MongoDB_Integration.py
@@ -1,6 +1,16 @@
 """
--> pip install -U autopep8 (package for auto formatting)
--> pip install django (install django package)
--> pip install djongo (mongodb for django)
--> pip install dnspython
+-> python -m venv venv
+        -> cd .\venv\Scripts
+        -> activate.bat (to activate virtual env using cmd)
+-> poetry init
+-> poetry add djangorestframework
+-> poetry add autopep8 (package for auto formatting)
+-> poetry add djongo (mongodb for django)
+-> poetry add dnspython
+
+*) to rename project folder as config:
+    -> https://youtu.be/mUPQii90K5A?t=278
+
+-> NOTE: we are not setting up users for this project because we are focusing on using mongodb
+
 """
