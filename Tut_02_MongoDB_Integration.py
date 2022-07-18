97b0ad1e7b3492e92e609ed52ab7cbd4631f7e34
Roman-Ojha <razzroman98@gmail.com>
2022-07-18 22:00:15 +0545

    build: initilized venv & django

---
+++ Tut_02_MongoDB_Integration.py
@@ -0,0 +1,6 @@
+"""
+-> pip install -U autopep8 (package for auto formatting)
+-> pip install django (install django package)
+-> pip install djongo (mongodb for django)
+-> pip install dnspython
+"""
